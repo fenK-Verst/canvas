d804ad0c43165cbecfc237527890af405ebdeb50
fenK <mihha1818@ya.ru>
2020-01-08 10:48:15 +0600

    last stable

---
+++ js/main.js
@@ -0,0 +1,516 @@
+let canvas = document.getElementById("canvas"),
+    ctx = canvas.getContext("2d"),
+    isMouseDown = false,
+    isMouseMoved = false,
+    bgColor = "#a2a2a2",
+    width = 40;
+canvas.width = window.innerWidth;
+canvas.height = window.innerHeight;
+canvas.style.display = "block";
+
+canvas.style.background = bgColor;
+
+ctx.fillStyle = "white";
+
+class Board {
+    figures = [];
+    lastSelectedFigure;
+    isSelectedFigure = false;
+    movedPart;
+    relations = [];
+    addFigure = (figure) => {
+        this.figures.push(figure);
+    };
+    deleteFigure = (figure = board.lastSelectedFigure) => {
+        this.removeRelations(figure);
+        this.figures.splice(this.figures.indexOf(figure), 1);
+        this.redraw();
+    };
+    findFigure = (x, y) => {
+        let value = false;
+        this.figures.forEach(figure => {
+            if (figure.isSelected(x, y)) value = figure;
+        });
+
+        return value;
+    };
+    redraw = () => {
+        ctx.clearRect(0, 0, canvas.width, canvas.height);
+        ctx.beginPath();
+
+        this.isSelectedFigure = false;
+        this.figures.forEach((figure) => {
+
+            for (let key in figure.sections) {
+                if (figure.sections[key]) {
+                    figure.sections[key].recount();
+
+                }
+            }
+            figure.draw();
+            // figure.drawRelations();
+            figure.sections.isDrawed = false;
+        });
+        this.drawRelations();
+        ctx.fill();
+        ctx.closePath();
+    };
+    moveSelectedFigureSection = (x, y) => {
+        let figure = this.lastSelectedFigure,
+            key = this.movedPart == "section" ? figure.lastSelectedSection : figure.getSection(x, y);
+        if (key) {
+            figure.lastSelectedSection = key;
+            this.redraw();
+            this.isSelectedFigure = true;
+            switch (key) {
+                case "top":
+                    x -= width / 2;
+                    break;
+                case "bot":
+                    y -= width;
+                    x -= width / 2;
+                    break;
+                case "left":
+                    y -= width / 2;
+                    break;
+                case "right":
+                    y -= width / 2;
+                    x -= width;
+                    break;
+            }
+
+            figure.sections[key].recount(x, y);
+            figure.drawSections();
+            this.figures.forEach(figure => {
+                figure.drawSections();
+            })
+        }
+    };
+    drawRelations = () => {
+        ctx.beginPath();
+        this.relations.forEach(relation => {
+            let figure,
+                coords = [];
+
+            relation.forEach((value, i) => {
+                figure = value.figure;
+                coords[i] = {};
+                switch (value.section) {
+                    case "top":
+                        coords[i].x = figure.sections["top"].coords[1].x;
+                        coords[i].y = figure.sections["top"].coords[0].y;
+                        break;
+                    case "bot":
+                        coords[i].x = figure.sections["bot"].coords[1].x;
+                        coords[i].y = figure.sections["bot"].coords[0].y;
+                        break;
+                    case "left":
+                        coords[i].x = figure.sections["left"].coords[0].x;
+                        coords[i].y = figure.sections["left"].coords[1].y;
+                        break;
+                    case "right":
+                        coords[i].x = figure.sections["right"].coords[0].x;
+                        coords[i].y = figure.sections["right"].coords[1].y;
+                        break;
+
+                }
+            });
+            ctx.moveTo(coords[0].x, coords[0].y);
+            ctx.lineTo(coords[1].x, coords[1].y);
+        });
+        ctx.stroke();
+
+        ctx.closePath();
+    };
+    removeRelations = (figure) => {
+        this.relations = this.relations.filter((relation, index) => {
+            if (relation[0].figure == figure || relation[1].figure == figure) {
+                relation[0].figure.relations[relation[0].section] = null;
+                relation[1].figure.relations[relation[1].section] = null;
+            } else {
+                return relation;
+            }
+
+
+        });
+    };
+
+}
+class Figure {
+    sections = {};
+    relations = {};
+    lastSelectedSection;
+
+    constructor(x, y, width) {
+        this.width = width;
+        this.coords = {
+            x: x,
+            y: y
+        };
+
+        ["left", "right", "top", "bot"].forEach(value => {
+            this.sections[value] = new Section(this, value);
+            this.relations[value] = null;
+        });
+
+        this.isDrawed = false;
+        this.draw();
+        Object.defineProperty(this.sections, "isDrawed", {
+            enumerable: false,
+            writable: true
+        });
+
+    };
+
+
+    draw = () => {
+        ctx.beginPath();
+        ctx.fillStyle = "white";
+        ctx.strokeStyle = "black";
+        ctx.fillRect(this.coords.x, this.coords.y, this.width, this.width);
+        ctx.stroke();
+        ctx.closePath();
+        this.isDrawed = true;
+    };
+    drawSections = () => {
+
+        ctx.beginPath();
+        ctx.fillStyle = "white";
+        ctx.strokeStyle = "black";
+        for (let key in this.sections) {
+            let section = this.sections[key];
+            section.draw();
+        }
+
+        this.sections.isDrawed = true;
+
+        ctx.fill();
+        ctx.stroke();
+        ctx.closePath();
+
+    };
+
+    getSection = (x, y) => {
+        let result = false;
+        /*
+        Математическая часть - векторное и псевдоскалярное произведения.
+    Реализация - считаются произведения (1, 2, 3 - вершины треугольника, 0 - точка):
+    (x1 - x0) * (y2 - y1) - (x2 - x1) * (y1 - y0)
+    (x2 - x0) * (y3 - y2) - (x3 - x2) * (y2 - y0)
+    (x3 - x0) * (y1 - y3) - (x1 - x3) * (y3 - y0)
+    Если они одинакового знака, то точка внутри треугольника, если что-то из этого - ноль, то точка лежит на стороне, иначе точка вне треугольника.
+         */
+
+        for (let key in this.sections) {
+            let th = this.sections[key].coords;
+
+            let a = (th[0].x - x) * (th[1].y - th[0].y) - (th[1].x - th[0].x) * (th[0].y - y),
+                b = (th[1].x - x) * (th[2].y - th[1].y) - (th[2].x - th[1].x) * (th[1].y - y),
+                c = (th[2].x - x) * (th[0].y - th[2].y) - (th[0].x - th[2].x) * (th[2].y - y);
+            //console.log((!Math.sign(obj[0]) && !Math.sign(obj[1]) && !Math.sign(obj[2])) == false);
+            a = Math.sign(a);
+            b = Math.sign(b);
+            c = Math.sign(c);
+
+            if (a == b && a == b && a == c) {
+                result = key;
+            }
+        }
+        return result;
+
+    };
+    isSelected = (x, y) => {
+        return (x >= this.coords.x && x <= this.coords.x + this.width && y >= this.coords.y && y <= this.coords.y + this.width);
+
+    };
+    move = (x, y) => {
+        // if (this.getSelected(x, y)) {
+
+        this.coords.x = x;
+        this.coords.y = y;
+        for (let key in this.sections) {
+            this.sections[key].recount();
+        }
+
+        board.redraw();
+        // }
+
+    };
+
+}
+class Section {
+
+    constructor(parent, type) {
+        this.type = type;
+        this.parent = parent;
+        this.isDrawed = false;
+        this.recount();
+
+    };
+
+    recount = (x = this.parent.coords.x, y = this.parent.coords.y) => {
+
+        width = this.parent.width;
+
+        switch (this.type) {
+            case "top":
+                this.coords = [
+                    {
+                        x: x,
+                        y: y
+                    },
+                    {
+                        x: x + width / 2,
+                        y: y - width / 2
+                    },
+                    {
+                        x: x + width,
+                        y: y
+                    }
+                ];
+                break;
+            case  "bot":
+                this.coords = [
+                    {
+                        x: x,
+                        y: y + width
+                    },
+                    {
+                        x: x + width / 2,
+                        y: y + width + width / 2
+                    },
+                    {
+                        x: x + width,
+                        y: y + width
+                    }
+                ];
+                break;
+            case  "left":
+                this.coords = [
+                    {
+                        x: x,
+                        y: y
+                    },
+                    {
+                        x: x - width / 2,
+                        y: y + width / 2
+                    },
+                    {
+                        x: x,
+                        y: y + width
+                    }
+                ];
+                break;
+            case  "right":
+                this.coords = [
+                    {
+                        x: x + width,
+                        y: y
+                    },
+                    {
+                        x: x + width + width / 2,
+                        y: y + width / 2
+                    },
+                    {
+                        x: x + width,
+                        y: y + width
+                    }
+                ];
+                break;
+            default:
+                throw "Invalid type";
+        }
+    };
+    draw = () => {
+
+        ctx.moveTo(this.coords[0].x, this.coords[0].y);
+        ctx.lineTo(this.coords[1].x, this.coords[1].y);
+        ctx.lineTo(this.coords[2].x, this.coords[2].y);
+
+    };
+    drawColored = () => {
+        ctx.beginPath();
+        ctx.fillStyle = "green";
+        this.draw();
+        ctx.fill();
+        ctx.stroke();
+        ctx.closePath();
+        ctx.fillStyle = bgColor;
+    };
+
+
+}
+
+let board = new Board();
+board.addFigure(new Figure(canvas.width / 2 - width, canvas.height / 2 - width, width));
+// board.addFigure(new Figure(400, 100, width));
+
+canvas.addEventListener("mousedown", function (e) {
+    isMouseDown = true;
+
+});
+canvas.addEventListener("mouseup", function (e) {
+    let x = e.clientX,
+        y = e.clientY;
+    isMouseDown = false;
+    if (board.movedPart === "section") {
+
+        board.figures.forEach(figure => {
+            if (figure.getSection(x, y) && figure != board.lastSelectedFigure) {
+                let relation = [
+                    {
+                        figure: figure,
+                        section: figure.getSection(x, y)
+
+                    },
+                    {
+                        figure: board.lastSelectedFigure,
+                        section: board.lastSelectedFigure.lastSelectedSection
+                    }
+                ];
+                board.lastSelectedFigure.relations[board.lastSelectedFigure.lastSelectedSection] = figure;
+                figure.relations[figure.getSection(x, y)] = board.lastSelectedFigure;
+                board.relations.push(relation);
+            }
+        });
+        board.redraw();
+    }
+
+    board.movedPart = null;
+    if (isMouseMoved) {
+        console.log("moved");
+    }
+    isMouseMoved = false;
+    // if (board.selectedDot) board.selectedDot.buttons.draw();
+});
+canvas.addEventListener("mousemove", function (e) {
+    let x = e.clientX,
+        y = e.clientY,
+        figure;
+    if (isMouseDown) {
+
+        figure = isMouseMoved ? board.lastSelectedFigure : board.findFigure(x, y);
+
+        if (figure && board.movedPart != "section") {
+            board.lastSelectedFigure = figure;
+            figure.move(x - figure.width / 2, y - figure.width / 2);
+            board.movedPart = "figure";
+            isMouseMoved = true;
+
+        } else if (board.isSelectedFigure && board.lastSelectedFigure && e.shiftKey) {
+            // board.lastSelectedFigure.lastSelectedSection.move(x,y);
+            board.moveSelectedFigureSection(x, y);
+            board.movedPart = "section";
+
+        } else {
+            board.movedPart = null;
+        }
+
+
+    } else if (board.isSelectedFigure && board.lastSelectedFigure) {
+        let section = board.lastSelectedFigure.getSection(x, y);
+        board.lastSelectedFigure.drawSections();
+        if (section) {
+            board.lastSelectedFigure.sections[section].drawColored();
+        }
+    }
+
+});
+canvas.addEventListener("click", function (e) {
+    let x = e.clientX,
+        y = e.clientY;
+    if (board.isSelectedFigure && board.lastSelectedFigure && board.lastSelectedFigure.getSection(x, y)) {
+        let selectedFigure = board.lastSelectedFigure,
+            relationFigureKey,
+            key = selectedFigure.getSection(x, y),
+            figureCoords = {
+                x, y
+            };
+
+        if (selectedFigure.relations[key] == null) {
+            switch (key) {
+                case "top":
+                    figureCoords.x = selectedFigure.coords.x;
+                    figureCoords.y = selectedFigure.coords.y - width * 3;
+                    relationFigureKey = "bot";
+                    break;
+                case "left":
+                    figureCoords.x = selectedFigure.coords.x - width * 3;
+                    figureCoords.y = selectedFigure.coords.y;
+                    relationFigureKey = "right";
+                    break;
+                case "right":
+                    figureCoords.x = selectedFigure.coords.x + width * 3;
+                    figureCoords.y = selectedFigure.coords.y;
+                    relationFigureKey = "left";
+                    break;
+                case "bot":
+                    figureCoords.x = selectedFigure.coords.x;
+                    figureCoords.y = selectedFigure.coords.y + width * 3;
+                    relationFigureKey = "top";
+                    break;
+
+            }
+            if (figureCoords.x > 0 &&
+                figureCoords.y > 0 &&
+                figureCoords.x < canvas.width - width &&
+                figureCoords.y < canvas.height - width
+
+            ) {
+                let may = true;
+                board.figures.forEach(figure => {
+                    if (figure.isSelected(figureCoords.x, figureCoords.y)) may = false;
+                });
+
+                if (may) {
+                    let figure = new Figure(figureCoords.x, figureCoords.y, width);
+                    board.addFigure(figure);
+
+                    selectedFigure.relations[key] = figure;
+                    figure.relations[relationFigureKey] = selectedFigure;
+                    board.relations.push([
+                        {
+                            figure: figure,
+                            section: relationFigureKey
+                        },
+                        {
+                            figure: selectedFigure,
+                            section: key
+                        }
+
+                    ]);
+
+                    board.redraw();
+                    board.drawRelations();
+                    board.lastSelectedFigure = figure;
+                    board.isSelectedFigure = true;
+                    figure.drawSections();
+
+                }
+            }
+        }
+    }
+
+});
+canvas.addEventListener("dblclick", function (e) {
+    let x = e.clientX,
+        y = e.clientY;
+
+    let figure = board.findFigure(x, y);
+
+    if (figure && !figure.sections.isDrawed) {
+        board.redraw();
+        board.isSelectedFigure = true;
+        figure.drawSections();
+        board.lastSelectedFigure = figure;
+        board.isSelectedFigure = true;
+    } else {
+        board.redraw();
+    }
+
+});
+document.addEventListener("keydown", function (e) {
+    if (e.keyCode === 46 && board.isSelectedFigure) {
+        board.deleteFigure();
+    }
+});
---
+++ js/test.js
@@ -0,0 +1,7 @@
+let a = [1,2,3,4,5,6];
+let b = a.forEach( (value,key) =>{
+
+
+});
+console.log(a);
+console.log(b);
